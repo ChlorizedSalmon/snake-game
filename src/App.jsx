54940566b5ba482ab70d502208f113d650350b68
daoml <mjy9088@gmail.com>
2019-09-13 00:51:16 +0900

    splash

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,13 @@ class App extends React.Component {
     render() {
         return (
             <div className="splash">
-                <h1>뱀 게임</h1>
+                <hgroup>
+                    <h1>뱀 게임</h1>
+                </hgroup>
+                <nav>
+                    <a>혼자 하기</a>
+                    <a>온라인</a>
+                </nav>
             </div>
         );
     }
