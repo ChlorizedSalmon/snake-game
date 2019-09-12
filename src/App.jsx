2139ed6857aec20fa10f4a5068da709bb8759c5a
daoml <mjy9088@gmail.com>
2019-09-12 13:48:29 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./styles/base.css";
+import "./styles/splash.scss";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="splash">
+                <h1>뱀 게임</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
